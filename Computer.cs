2e8621d01921ee583074d88ec1bf61af7a1cda2c
DanielLevi3 <66633430+DanielLevi3@users.noreply.github.com>
2020-06-16 17:33:00 +0300

    Add files via upload

---
+++ Computer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_work_140620
+{
+    
+   [DebuggerDisplay("{model} {(price*3.67)}")]
+   public class Computer
+    {
+
+        public string model;
+        public int price;
+        public int numberOfProcessors;
+        public float screenSize;
+        public bool isTurnOn = false;
+
+
+        public void TellMeThePrice()
+        {
+            Console.WriteLine($"My price is {price}");
+        }
+        public void TellMeTheScreenSize()
+        {
+            Console.WriteLine($"My screen size is {screenSize}");
+        }
+        public void TurnOn()
+        {
+            Console.WriteLine("The computer is on");
+            isTurnOn = true;
+        }
+        public void TurnOFF()
+        { 
+            Console.WriteLine("The computer is off ");
+            isTurnOn = false;
+        }
+        public int AddProcessor()
+        {
+            return numberOfProcessors;
+        }
+        public override string ToString()
+        {
+            return $"[Computer] : model:{model} price:{price} numberOfProcessors:{numberOfProcessors} screenSize:{screenSize} isTurnOn:{isTurnOn}";
+        }
+    }
+}
